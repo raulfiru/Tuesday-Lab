0de33363bd5cba35d19464108a32cb0e0fc5378a
Raul Firu <raulfiru@gmail.com>
2017-10-03 09:30:57 +0300

    added .NET sources

---
+++ Session1/Code/.NET/console/app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using library;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! "+Calculator.Add(5,6));
+        }
+    }
+}
---
+++ Session1/Code/.NET/console/library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace library
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
